a9591003100727f3a5d056159c607068df3f796b
Thiyansa Ravidu <93363542+Thiyansa@users.noreply.github.com>
2026-05-06 18:37:10 +0530

    Update install script & Remove Hystiria

--- lunes-host/install.sh
+++ lunes-host/install.sh
@@ -3,10 +3,9 @@
 DOMAIN="${DOMAIN:-node68.lunes.host}"
 PORT="${PORT:-10008}"
 UUID="${UUID:-2584b733-9095-4bec-a7d5-62b473540f7a}"
-HY2_PASSWORD="${HY2_PASSWORD:-vevc.HY2.Password}"
 
-curl -sSL -o app.js https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/lunes-host/app.js
-curl -sSL -o package.json https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/lunes-host/package.json
+curl -sSL -o app.js https://raw.githubusercontent.com/Thiyansa/one-node/refs/heads/main/lunes-host/app.js
+curl -sSL -o package.json https://raw.githubusercontent.com/Thiyansa/one-node/refs/heads/main/lunes-host/package.json
 
 mkdir -p /home/container/xy
 cd /home/container/xy
@@ -14,7 +13,7 @@ curl -sSL -o Xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/downlo
 unzip Xray-linux-64.zip
 rm Xray-linux-64.zip
 mv xray xy
-curl -sSL -o config.json https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/lunes-host/xray-config.json
+curl -sSL -o config.json https://raw.githubusercontent.com/Thiyansa/one-node/refs/heads/main/lunes-host/xray-config.json
 sed -i "s/10008/$PORT/g" config.json
 sed -i "s/YOUR_UUID/$UUID/g" config.json
 keyPair=$(./xy x25519)
@@ -26,21 +25,8 @@ sed -i "s/YOUR_SHORT_ID/$shortId/g" config.json
 vlessUrl="vless://$UUID@$DOMAIN:$PORT?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.cloudflare.com&fp=chrome&pbk=$publicKey&sid=$shortId&spx=%2F&type=tcp&headerType=none#lunes-reality"
 echo $vlessUrl > /home/container/node.txt
 
-mkdir -p /home/container/h2
-cd /home/container/h2
-curl -sSL -o h2 https://github.com/apernet/hysteria/releases/download/app%2Fv2.6.2/hysteria-linux-amd64
-curl -sSL -o config.yaml https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/lunes-host/hysteria-config.yaml
-openssl req -x509 -newkey rsa:2048 -days 3650 -nodes -keyout key.pem -out cert.pem -subj "/CN=$DOMAIN"
-chmod +x h2
-sed -i "s/10008/$PORT/g" config.yaml
-sed -i "s/HY2_PASSWORD/$HY2_PASSWORD/g" config.yaml
-encodedHy2Pwd=$(node -e "console.log(encodeURIComponent(process.argv[1]))" "$HY2_PASSWORD")
-hy2Url="hysteria2://$encodedHy2Pwd@$DOMAIN:$PORT?insecure=1#lunes-hy2"
-echo $hy2Url >> /home/container/node.txt
-
 echo "============================================================"
 echo "🚀 VLESS Reality & HY2 Node Info"
 echo "------------------------------------------------------------"
 echo "$vlessUrl"
-echo "$hy2Url"
 echo "============================================================"
