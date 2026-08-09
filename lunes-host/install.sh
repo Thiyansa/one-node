0fd6eca714fd42f52914a5b8f586be8f870944a8
Thiyansa Ravidu <93363542+Thiyansa@users.noreply.github.com>
2026-08-09 09:54:49 +0530

    Update install.sh

--- lunes-host/install.sh
+++ lunes-host/install.sh
@@ -10,7 +10,7 @@ curl -sSL -o index.html https://raw.githubusercontent.com/Thiyansa/nodejs-vless/
 
 mkdir -p /home/container/xy
 cd /home/container/xy
-curl -sSL -o Xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/download/v25.8.3/Xray-linux-64.zip
+curl -sSL -o Xray-linux-64.zip https://github.com/Thiyansa/one-node/releases/download/lunes-host-xray/Xray-linux-64.zip
 unzip Xray-linux-64.zip
 rm Xray-linux-64.zip
 mv xray xy
